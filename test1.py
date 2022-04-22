ebf1d297a5cf57bb1708734d7050e41c7af5b0c8
apodxx <apodx1997@gmail.com>
2022-04-22 14:15:32 +0800

    first commit

---
+++ test1.py
@@ -0,0 +1,31 @@
+def mkdir(path):
+    # 引入模块
+    import os
+    
+    # 去除首位空格
+    path=path.strip()
+    # 去除尾部 \ 符号
+    path=path.rstrip("\\")
+ 
+    # 判断路径是否存在
+    # 存在     True
+    # 不存在   False
+    isExists=os.path.exists(path)
+ 
+    # 判断结果
+    if not isExists:
+        # 如果不存在则创建目录
+        # 创建目录操作函数
+        os.makedirs(path) 
+ 
+        print(path+' 创建成功')
+        return True
+    else:
+        # 如果目录存在则不创建，并提示目录已存在
+        print (path+' 目录已存在')
+        return False
+ 
+# 定义要创建的目录
+mkpath="../result/"
+# 调用函数
+mkdir(mkpath)
